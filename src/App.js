b044751d576a3c446186a8f7750eb722147f944b
Nuclear One <ajithsnair10@gmail.com>
2022-04-02 13:21:44 +0530

    Add assending & desending order button and React App with optimized way

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback} from 'react';
 
 import './App.css';
 import DemoList from './Components/Demo/DemoList';
 import Button from './Components/UI/Button/Button';
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
+  const listItems =  [5, 3, 1, 10, 9];
+  const [listTitle, setListTitle] = useState('Un orderd List');
+  const [NewOrderState, setOrderState] = useState(listItems);
 
-  const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
-  }, []);
+  const sortAssendingHandler = useCallback(() => {
+    const AssendingSortedList = listItems.sort((a, b) => a - b);
+      console.log('Assending order List');
+    setListTitle('Assending order List')
+    setOrderState(AssendingSortedList);
+  },[]);
 
-  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+  const sortDesendingHandler =useCallback( () => {
+    const DessendingSortedList = listItems.sort((a, b) => b - a);
+      console.log('Dessending order List');
+    setListTitle('Dessending order List')
+    setOrderState(DessendingSortedList);
+  },[]);
 
   return (
     <div className="app">
-      <DemoList title={listTitle} items={listItems} />
-      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <DemoList title={listTitle} items={NewOrderState} />
+      <Button onClick={sortAssendingHandler}>Assending Order</Button>
+      <Button onClick={sortDesendingHandler}>Descending Order</Button>
     </div>
   );
 }
--- src/Components/Demo/DemoList.js
+++ src/Components/Demo/DemoList.js
@@ -3,19 +3,13 @@ import React, { useMemo } from "react";
 import classes from "./DemoList.module.css";
 
 const DemoList = (props) => {
-  const { items } = props;
-
-  const sortedList = useMemo(() => {
-    console.log("Items sorted");
-    return items.sort((a, b) => a - b);
-  }, [items]);
   console.log("DemoList RUNNING");
 
   return (
     <div className={classes.list}>
       <h2>{props.title}</h2>
       <ul>
-        {sortedList.map((item) => (
+          {props.items.map((item) => (
           <li key={item}>{item}</li>
         ))}
       </ul>
